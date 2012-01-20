4c6dc03af2c16aba3b7178959e81ea8e8695b3c0
Junichi Ito <june.1st.717@gmail.com>
2012-01-20 16:21:46 +0900

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :user_name
-  validates :user_name, :presence => true
+
+  user_name_regex = /^[a-z0-9]+$/
+  validates :user_name, 
+            :presence => true, 
+            :length => { :maximum => 20 },
+            :format => { :with => user_name_regex }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe User do
@@ -12,5 +13,19 @@ describe User do
   it "should require a name" do
     no_user_name_user = User.new
     no_user_name_user.should_not be_valid
+  end
+
+  it "should reject user_names that are too long" do
+    long_name = "a" * 21
+    long_name_user = User.new(@attr.merge(:user_name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should reject invalid user_names" do
+    names = ['UpperCase', 'Include Space', 'にほんご', 'symbol#$%etc']
+    names.each do |name|
+      invalid_user_name_user = User.new(@attr.merge(:user_name => name))
+      invalid_user_name_user.should_not be_valid
+    end
   end
 end
