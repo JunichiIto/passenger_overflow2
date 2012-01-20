26d197f482005d94a97c66eb3f2dc0846676a068
Junichi Ito <june.1st.717@gmail.com>
2012-01-20 17:33:49 +0900

    Implemented basic sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome!"
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -43,5 +43,27 @@ describe UsersController do
         response.should render_template('new')
       end
     end
+
+    describe "success" do
+      before(:each) do
+        @attr = { :user_name => "junichiito" } 
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+    
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome/i
+      end
+    end
   end
 end
