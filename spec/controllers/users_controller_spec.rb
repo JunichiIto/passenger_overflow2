e7f65eeb5410ac7e9a8e90e5930465446240be4e
Junichi Ito <june.1st.717@gmail.com>
2012-01-20 18:01:19 +0900

    basic sign in form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,6 +13,11 @@ describe UsersController do
       response.should be_success
     end
 
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("h2", :content => "User")
+    end
+
     it "should find the right user" do
       get :show, :id => @user
       assigns(:user).should == @user
@@ -23,6 +28,11 @@ describe UsersController do
     it "should be successful" do
       get 'new'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'new'
+      response.should have_selector("h2", :content => "Sign up")
     end
   end
 
