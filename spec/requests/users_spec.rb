0718ff6d264235930c54164019a7dd13b0fed26e
Junichi Ito <june.1st.717@gmail.com>
2012-01-20 17:39:32 +0900

    added integ test

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Users" do
+  describe "signup" do
+    describe "failure" do
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "User name", :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(User, :count)
+      end
+    end
+
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "User name", :with => "junichiito"
+          click_button
+          response.should have_selector("div.flash.success",
+                                        :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+end
