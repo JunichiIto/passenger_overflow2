71b516f33cad76ea6c00878c6b10b1cf25a56531
Junichi Ito <june.1st.717@gmail.com>
2012-01-22 13:25:02 +0900

    added validation on ans

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,5 +4,9 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   
+  validates :content, :presence => true
+  validates :user_id, :presence => true
+  validates :question_id, :presence => true
+
   default_scope :order => 'answers.created_at DESC'
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 describe Answer do
   before :each do
     @user = Factory :user 
-    asker = Factory :user, :user_name => 'someone'
-    @question = Factory :question, :user => asker
+    @asker = Factory :user, :user_name => 'someone'
+    @question = Factory :question, :user => @asker
   end
 
   it "should create a new instance given valid attributes" do
-    answer = @user.answers.build question: @question
+    answer = @user.answers.build question: @question, content: "my ans"
+    @question.answers << answer
     answer.save!
   end
 
@@ -33,6 +34,28 @@ describe Answer do
 
     it "should have a question attribute" do
       @answer.should respond_to :question
+    end
+  end
+
+  describe "validations" do
+    before :each do
+      @attr = { content: "my ans", user: @user, question: @question }
+    end
+
+    it "should require a user id" do
+      no_user = Answer.new(@attr.merge(user: nil))
+      no_user.should_not be_valid
+    end
+
+    it "should require a question id" do
+      no_question = Answer.new(@attr.merge(quetion: nil))
+      no_question.should_not be_valid
+    end
+
+    it "should require nonblank content" do
+      #@user.microposts.build(:content => "  ").should_not be_valid
+      no_content = Answer.new(@attr.merge(content: "  "))
+      no_content.should_not be_valid
     end
   end
 end
