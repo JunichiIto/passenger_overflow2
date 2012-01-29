371d8ff4589efd6cbadc93ffb864a326862dbe90
Junichi Ito <june.1st.717@gmail.com>
2012-01-29 19:44:56 +0900

    added link on user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def render_user(user)
-    user.user_name + " " + user.reputation_point.to_s
+    link_to(user.user_name, user) + " " + user.reputation_point.to_s
   end
 end
