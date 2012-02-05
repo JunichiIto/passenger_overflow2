174b9cb2b85428ca7191ed74883ebb8cb69d1664
Junichi Ito <june.1st.717@gmail.com>
2012-02-05 19:35:57 +0900

    refactor model info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def render_model_info(model)
+    ("[" +
+    model.created_at.to_s +
+    " " +
+    render_user(model.user) +
+    "]").html_safe
+  end
+
   def render_user(user)
     link_to(user.user_name, user) + " " + user.reputation_point.to_s
   end
