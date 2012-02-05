a43367f01d8a7d1ac78211fa8e0c45b4c73d10e7
Junichi Ito <june.1st.717@gmail.com>
2012-02-06 05:00:36 +0900

    refactored views

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,63 +1,7 @@
 module AnswersHelper
-  def render_accept_section(answer)
-    if @question.accepted?
-      if @question.accepted_answer_id == answer.id
-        render_accepted
-      end
-    else
-      if current_user == @question.user
-        content_tag :span, id: "accept#{answer.id}" do
-          link_to "Accept", 
-                  accept_answer_path(answer), 
-                  method: :post, 
-                  remote: true
-        end
-      end
-    end
-  end
-
-  def render_accepted
-    content_tag :span, "Accepted", class: "accepted"
-  end
-
-  def render_vote_count(answer)
-    if signed_in? && current_user.already_voted?(answer)
-      content_tag :span, 
-                  pluralize(answer.votes.count, "vote"), 
-                  class: "voted", 
-                  id: "votecnt#{answer.id}"
-    else
-      content_tag :span, 
-                  pluralize(answer.votes.count, "vote"), 
-                  id: "votecnt#{answer.id}"
-    end
-  end    
-  
   def can_vote?(answer)
     signed_in? && 
     !current_user.already_voted?(answer) && 
     answer.user != current_user
-  end
-
-  def render_answer_count(question)
-    if question.accepted?
-       content_tag :span, 
-                    pluralize(question.answers.count, "Answer"), 
-                    class: "accepted"
-    else
-      pluralize question.answers.count, "Answer"
-    end
-  end
-
-  def render_vote_link(answer)
-    if can_vote? answer
-      content_tag :span, id: "votelink#{answer.id}" do
-        link_to "Vote+", 
-                vote_answer_path(answer), 
-                method: :post, 
-                class: "vote", 
-                remote: true 
-      end
-    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def render_model_info(model)
-    "[ #{model.created_at.to_s} #{render_user(model.user)} ]".html_safe
-  end
-
-  def render_user(user)
-    "#{link_to(user.user_name, user)} #{user.reputation_point.to_s}"
-  end
+#  def render_model_info(model)
+#    "[ #{model.created_at.to_s} #{render_user(model.user)} ]".html_safe
+#  end
+#
+#  def render_user(user)
+#    "#{link_to(user.user_name, user)} #{user.reputation_point.to_s}"
+#  end
 end
